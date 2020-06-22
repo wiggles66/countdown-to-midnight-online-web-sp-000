40884efa549454b749fb639b5567794713aace6f
Michael Waide <mvwaide@gmail.com>
2020-06-22 16:25:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,5 @@
 def countdown(number)
+<<<<<<< HEAD
   while number > 0
    sleep 1
     puts "#{number} SECOND(S)!"
@@ -8,3 +9,12 @@ def countdown(number)
 end
 
 countdown(10)
+=======
+  number = 10
+   while number > 0
+     puts "#{number} SECOND(S)!"
+     number -= 1
+   break if number <= 0
+    "HAPPY NEW YEAR!"
+  end
+>>>>>>> 08ba01d27eb8d362e9070e8af3ab1e64a5f3015a
